ecc5f7056e27fc13d823f9f15df1ecbfd3f482ac
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-20 12:18:21 +0100

    fixed user pb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  # validates :name, presence: true
-  validates :category, presence: true, inclusion: { in: %w(doudou papiers bijoux vetements electronique divers)}
+  validates :category, presence: true
   validates :date_found, presence: true
   validates :location, presence: true
-  # validates :photo_id, presence: true
 end
