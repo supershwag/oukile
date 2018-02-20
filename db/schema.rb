0031f302f9f69a88495f710dffe946333236e7e1
Thanh an Huynh <thanh1606@gmail.com>
2018-02-20 16:48:58 +0100

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -42,11 +42,8 @@ ActiveRecord::Schema.define(version: 20180220094335) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "phone_number"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
