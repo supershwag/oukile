a40ff19b11dab624cfd60bbe3f6389c082033512
tom vaillant <chroniclr@hushmail.com>
2018-02-20 18:41:02 +0100

    navbar, devise redirects add

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def after_sign_up_path_for(resource)
+    user_path(resource) # Or :prefix_to_your_route
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations"}
   resources :users, only: [:edit, :update, :show]
   resources :meetings
   # For details on the DSL available within this filegit, see http://guides.rubyonrails.org/routing.html
