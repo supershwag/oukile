8e00146666ec3fc997037f644d24dc3e63c36553
tom vaillant <chroniclr@hushmail.com>
2018-02-20 12:14:01 +0100

    object change to item

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,7 +1,7 @@
 class Meeting < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
-  belongs_to :object
-  has_one :user, through: :object
+  belongs_to :item
+  has_one :user, through: :item
 
   validates :start_date, :end_date, presence: true
 end
