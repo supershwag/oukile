355a0e513d1bb3128a126fa5f41bc7f2b37c67b1
tom vaillant <chroniclr@hushmail.com>
2018-02-20 14:53:27 +0100

    meetings controller fixes, user show working, user controller -> users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 
 def show
+  @user = current_user
 end
 
 def edit
@@ -8,7 +9,7 @@ def edit
 end
 
 def update
-    current_user.update(user_params)
+  current_user.update(user_params)
 end
 
 private
