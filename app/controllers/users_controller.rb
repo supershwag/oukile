dba26f39477acb7425f459f088db3c8a5ccc8335
tom vaillant <chroniclr@hushmail.com>
2018-02-21 11:36:19 +0100

    photo-uploader users and items, navbar html tweaks, sign up form amendments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ end
 private
     # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :phone_number)
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :photo)
   end
 
 end
