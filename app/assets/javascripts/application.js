18fd6681f393c3092a1b2778c541523ca06e36c6
tom vaillant <chroniclr@hushmail.com>
2018-02-21 14:44:23 +0100

    modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
 //= require rails-ujs
 //= require_tree .
 
+const meetingButtonClick = document.getElementById("set-meeting")
+const launchMeeting = meetingButtonClick.addEventListener("click", (event) => {
+    event.currentTarget.innerText = "Bingo!";
+    event.currentTarget.classList.add("disabled");
+    audio.play();
+  });
