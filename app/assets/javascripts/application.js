300e46543263743c83f766ce161cf33d3a130324
tom vaillant <chroniclr@hushmail.com>
2018-02-21 15:09:17 +0100

    nav image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,27 @@
 //= require rails-ujs
 //= require_tree .
 
-const meetingButtonClick = document.getElementById("set-meeting")
-const launchMeeting = meetingButtonClick.addEventListener("click", (event) => {
-    event.currentTarget.innerText = "Bingo!";
-    event.currentTarget.classList.add("disabled");
-    audio.play();
-  });
+const modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+const btn = document.getElementById("set-meeting");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
