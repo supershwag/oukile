2ae5cbdad5924f98e7274ee4601f422258d05971
Alex Deval <al3x.dvl@gmail.com>
2018-02-22 17:48:23 +0100

    datepicker + localization

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,10 @@
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import "flatpickr/dist/themes/confetti.css"// Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
+const French = require("flatpickr/dist/l10n/fr.js").default.fr;
+// or import { Russian } from "flatpickr/dist/l10n/ru.js"
+flatpickr.localize(French); // default locale is now Russian
 
 
 flatpickr(".datepicker", {
@@ -11,6 +14,10 @@ flatpickr(".datepicker", {
 } )
 
 flatpickr(".range", {
-  mode: "range"
+  mode: "range",
+  minDate: "today",
+  altInput: true,
+  altFormat: "j F Y",
+  dateFormat: "d-m-Y"
 } )
 
